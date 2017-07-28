90edd29799ad1f1c2921c2e4c3f39c9651f9a433
Valery Toda <ValeryToda@users.noreply.github.com>
2017-07-28 02:51:29 +0200

    Initial version

---
+++ data.py
@@ -0,0 +1,251 @@
+import scipy.ndimage as skimg
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import cv2
+import csv
+import matplotlib.pyplot as plt
+import random
+from utils import *
+
+USE_GRAY_IMAGES = True
+CORRECTION_DELTA = 0.2
+LEFT_IMAGES = []
+RIGHT_IMAGES = []
+CENTER_IMAGES = []
+Y_CENTER = []
+Y_LEFT = []
+Y_RIGHT = []
+
+
+# flipping image and steering measurement
+is_gray_mode = lambda: USE_GRAY_IMAGES
+flip_vertically = lambda image, angle: [np.fliplr(image), -1*angle]
+
+#During training, you want to feed the left and 
+#right camera images to your model as if they were 
+#coming from the center camera. This way, you can teach 
+#your model how to steer if the car drifts off to the 
+#left or the right. Figuring out how much to add or 
+#subtract from the center angle will involve some experimentation
+def rgb2gray(img):
+    # convert image from rgb to grayscale 
+    img = np.dot(img[...,:3], [0.299, 0.587, 0.114])
+    img = img.reshape(img.shape + (1,))
+    return img
+
+def create_random_shadow(img):
+    # Simulate shadow due to sun on road
+    # calculate the coordinates with respect 
+    # to the formula y = mx + b
+    # m = delta_y/delta_x; delta_x = distance(p1, p2) = p2 - p1
+    # at (p2, 0) b = - m*x 
+    height, width = img.shape[0], img.shape[1]
+    
+    # randomly choose p1 and p2
+    [p1, p2] = np.random.permutation(np.arange(width))[:2]
+    # calculate m 
+    m = height / (p2 - p1)
+    # calculate b with point of coordinate (p2, 0)
+    b = - m*p1
+    # change the pixel intensity at relevant coordinates
+    for y in range (height):
+        # calculate the corresponding x coordinate
+        x = int((y - b) / m)
+        # change the pixel intensity accordingly
+        img[y, :x, :] = (img[y, :x, :] * .39).astype(np.int32)
+    return img
+
+def load_data (abs_filepath):
+    with open(abs_filepath) as file:
+        f_handle = csv.reader(file) 
+        for line in f_handle:
+            LEFT_IMAGES.append(line[0].strip())
+            CENTER_IMAGES.append(line[1].strip())
+            RIGHT_IMAGES.append(line[2].strip())
+
+            angle = float(line[3])
+            Y_CENTER.append(angle)
+            Y_RIGHT.append(angle - CORRECTION_DELTA)
+            Y_LEFT.append(angle + CORRECTION_DELTA)
+
+def find_peaks(input_list, min_to_neighbors=1.8, min_to_max_value=0.05):
+    #adapted solution from https://stackoverflow.com/a/38521078
+    #find peaks in array with value bigger that the min_to_neighbors 
+    #(default = 180%) of that the neighbors and bigger that the 
+    #min_to_max_value (default = 5%) of the max value 
+    
+    in_arr = np.array(input_list)
+    max_in_arr = np.max(in_arr)
+    len_list = len(input_list)
+    result = []
+    for i in range(len_list):
+        is_peak = True
+        if i-1 > 0:
+            is_peak &= (in_arr[i] > min_to_neighbors * in_arr[i-1])
+            
+        if i+1 < len_list:
+            is_peak &= (in_arr[i] > min_to_neighbors * in_arr[i+1])
+
+        is_peak &= (in_arr[i] > min_to_max_value * max_in_arr)
+        if is_peak:
+            result.append(i)
+    return result
+
+def balance_data(datafiles, plot_data=False, reduce_factor=0.0):
+    # datafiles: array containing csv fílepaths
+
+    for datafile in datafiles:
+        load_data(datafile)
+    data = Y_LEFT + Y_CENTER + Y_RIGHT
+    n, b, _ = plt.hist(data, bins=50, linewidth=0.2)
+    if plot_data:
+        plt.figure(1)
+        plt.show()
+    
+    # retrieve the indices of bins
+    indices_of_bins = np.digitize(data, b)
+    #print ("indices_of_bins: {}".format(len(indices_of_bins)))
+    # find the unique values of bins
+    arr_unique = np.unique(indices_of_bins)
+    #print ("arr_unique: {}".format(arr_unique))
+    # count the occurence of unique values of bins
+    f = lambda a, v: a.tolist().count(v)
+    occurence = [f(indices_of_bins, value) for value in arr_unique]
+    #print ("occurences in bins: {}".format(occurence))
+
+    # find the peaks within the data
+    peaks = find_peaks(occurence)
+
+    # create a dataframe with all values
+    df_left = pd.DataFrame({"images": LEFT_IMAGES, "orientation": ['left' for index in range(len(LEFT_IMAGES))], "angles": Y_LEFT})
+    df_right = pd.DataFrame({"images": RIGHT_IMAGES, "orientation": ['right' for index in range(len(RIGHT_IMAGES))], "angles": Y_RIGHT})
+    df_center = pd.DataFrame({"images": CENTER_IMAGES, "orientation": ['center' for index in range(len(CENTER_IMAGES))], "angles": Y_CENTER})
+
+    _data = [df_left, df_center, df_right]
+    df = pd.concat(_data)
+    #:indices_of_bins}
+    df["bindices"] = pd.Series(indices_of_bins)
+
+    # drop unwanted peaks
+    df = df.drop(df[(-0.23 < df['angles']) & (df['angles'] < -0.19)].sample(frac=0.6).index)
+    df = df.drop(df[(0.19 < df['angles']) & (df['angles'] < 0.24)].sample(frac=0.6).index)
+    df = df.drop(df[(-1. < df['angles']) & (df['angles'] < -0.74)].sample(frac=0.9).index)
+    df = df.drop(df[(0.74 < df['angles']) & (df['angles'] < 1.0)].sample(frac=0.9).index)
+    df = df.drop(df[(-1.25 < df['angles']) & (df['angles'] < -1.0)].sample(frac=1.0).index)
+    df = df.drop(df[(1.0 < df['angles']) & (df['angles'] < 1.28)].sample(frac=1.0).index)
+
+    # Reduce size of df, to check if the whole network works 
+    if reduce_factor > 0.0:
+        for indx in np.unique(df['bindices'].values):
+            df = df.drop(df[df['bindices'] == indx].sample(frac=reduce_factor).index)
+   
+    plt.figure(num=2)
+    _, _, _ = plt.hist(list(df["angles"]), bins=50, linewidth=0.2)
+
+    if plot_data:
+        plt.show()
+    return df
+
+def random_adjust_image(img):  
+    # convert image to hsv
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV) 
+    # split color channnels
+    h, s, v = cv2.split(hsv_img)
+
+    mul_factor = np.random.uniform(0.7, 1.4)
+    np.multiply(v, mul_factor, out=v, casting='unsafe')
+    mul_factor = np.random.uniform(0.7, 1.4)
+    np.multiply(s, mul_factor, out=v, casting='unsafe')
+
+    add_factor = np.random.uniform(-0.1, 0.1)
+    np.add(v, add_factor, out=v, casting='unsafe')
+    add_factor = np.random.uniform(-0.1, 0.1)
+    np.add(s, add_factor, out=s, casting='unsafe')
+    
+    hsv_ = cv2.merge((h, s, v))
+    result = cv2.cvtColor(hsv_, cv2.COLOR_HSV2RGB)
+    return result
+
+
+def normalize_pixel_values(img, gray=USE_GRAY_IMAGES):
+    # Zero center and normalize the pixel values
+    if gray:
+        img = rgb2gray(img)
+        img -= np.mean(img)
+        img /= np.std(img)
+    else:
+        img = img/255.0 - 1.0
+    return img
+
+def crop_and_resize(image, plot=False):
+    cropped_img = image[70:130, :, :]
+    resize_img = cv2.resize(cropped_img, (64, 16))
+    if plot:
+        plt.imshow(resize_img)
+        plt.show()
+    return resize_img
+
+def pre_process_image(image):
+    return normalize_pixel_values(crop_and_resize(image))
+
+def load_all_data(samples):
+    """
+    samples: pf.Dataframe ['images', 'angles]
+    imports data from the source folder and 
+    returns X_train and y_train
+    """
+    images = []
+    angles = []
+    for index, row in samples.iterrows():
+        images.append(pre_process_image(skimg.imread(row['images'])))
+        angles.append(row['angles'])
+    return np.array(images), np.array(angles)
+
+@threadsafe_generator
+def generator(samples, batch_size=256):
+    """
+    samples: pf.Dataframe ['images', 'angles]
+    """
+    num_samples = len(samples)
+
+    while True:
+        samples = shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = shuffle(samples[offset:offset+batch_size])
+            images = []
+            angles = []
+            for _, row in batch_samples.iterrows():    
+                # steering angle and associated image
+                ang = row['angles']
+                img = skimg.imread(row['images'])
+                
+                angles.append(ang)
+                images.append(pre_process_image(img))
+                # add flipped image if random value == 2
+                rand_value = random.randint(1, 3)
+                if rand_value == 1:
+                    # associated image
+                    if row['orientation'] == "left" or row['orientation'] == "right":
+                        f_image, f_angle = flip_vertically(create_random_shadow(img), ang)
+                        angles.append(f_angle)
+                        images.append(pre_process_image(f_image))
+                    else:
+                        angles.append(ang)
+                        images.append(pre_process_image(create_random_shadow(img)))    
+
+                elif rand_value == 2:
+                    angles.append(ang)
+                    images.append(pre_process_image(random_adjust_image(img)))
+                
+                elif rand_value == 3:
+                    if row['orientation'] == "left" or row['orientation'] == "right":
+                        f_image, f_angle = flip_vertically(img, ang)
+                        angles.append(f_angle)
+                        images.append(pre_process_image(f_image))
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            
+            yield shuffle(X_train, y_train)
---
+++ model.py
@@ -0,0 +1,207 @@
+import tensorflow as tf
+from keras.models import Sequential
+import keras.backend as K
+from keras.layers import Dense, Conv2D, Flatten, Lambda, Cropping2D, Dropout, BatchNormalization, MaxPool2D, Activation
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import LearningRateScheduler
+from keras import initializers
+from keras import regularizers
+from keras import optimizers
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras.models import model_from_json
+from collections import namedtuple
+import matplotlib.pyplot as plt
+import csv
+import cv2
+import pickle
+import numpy as np
+import math
+from data import balance_data, generator, is_gray_mode
+from utils import threadsafe_iter
+
+train_generator = None
+validation_generator = None
+
+def plot_loss(history_object):
+    plt.figure(num=30)
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
+
+def save_model_topology(model):
+    # serialize keras model into JSON file
+    model_json = model.to_json()
+    with open("./_cnnet/model_topology.json", "w") as json_file:
+        json_file.write(model_json)
+    print("Model saved.")
+
+def load_model(json_filepath, weights_filepath, save_binary=False):
+    # load weights into a model and 
+    # save binary version 
+    
+    # load json and create model
+    json_file = open(json_filepath, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(weights_filepath)
+    #./_weights/w-10-0.20.hdf5
+    if save_binary:
+        loaded_model.save('./_cnnet/model.h5')
+    return loaded_model
+
+
+def nvidia_model(in_shape):
+    ## model derived from nvidia model architecture
+    # https://arxiv.org/pdf/1604.07316v1.pdf
+    # Dropouts and RELUs added
+
+    model = Sequential()
+    model.add(Conv2D(24, (5, 5), input_shape=in_shape, name='cv_1'))
+    model.add(Conv2D(36, (5, 5), strides=(2, 2), padding='same', name='cv_2'))
+    
+    model.add(BatchNormalization(name='bn_00'))    
+    model.add(Dropout(0.5, name='do_00'))    
+    model.add(Conv2D(48, (5, 5), strides=(2, 2), padding='same', name='cv_3'))
+
+    model.add(BatchNormalization(name='bn_01'))    
+    model.add(Dropout(0.5, name='do_01'))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', name='cv_4'))
+
+    model.add(BatchNormalization(name='bn_0'))    
+    model.add(Dropout(0.5, name='do_0'))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same', name='cv_5'))
+    
+    model.add(Flatten(name='ft_1'))
+
+    model.add(BatchNormalization(name='bn_0_0'))    
+    model.add(Dropout(0.5, name='do_0_0'))
+    model.add(Dense(100, name='fc_1'))
+    model.add(Activation('relu'))
+
+    model.add(BatchNormalization(name='bn_1'))
+    model.add(Dropout(0.25, name='do_1'))
+    model.add(Dense(50, name='fc_2'))
+    model.add(Activation('relu'))
+    
+    model.add(BatchNormalization(name='bn_2'))
+    model.add(Dropout(0.1, name='do_2'))
+    model.add(Dense(10, name='fc_3'))
+    model.add(Activation('relu'))
+
+    model.add(Dense(1, name='out'))
+    return model
+
+
+def train(hyper_params, ropl=True, resume=[]):
+    """
+    Training routine
+    @param hyper_params: namedtuple with hyper params configuration
+    @param gray: choose between training with colored or grayscaled images
+    """
+    K.clear_session()
+
+    if len(resume) > 0:
+        model = load_model(resume[0], resume[1])
+    else:
+        model = nvidia_model(hyper_params.in_shape)
+
+    # for SGD optimizer
+    model.compile(optimizer=optimizers.Adam(lr=hyper_params.learning_rate), loss='mean_squared_error', metrics=['accuracy'])
+    ## Model Summary
+    model.summary()
+    ## Save the model topology
+    save_model_topology(model)
+    ## Checkpoint
+    filepath="./_weights/w-{epoch:02d}-{val_loss:.2f}.hdf5"
+    checkpointer = ModelCheckpoint(filepath, monitor='val_loss', verbose=1,\
+                                    save_best_only=True)
+
+    # reduce the learnrate on plateau during training 
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=0.1e-6, verbose=1)
+    
+    # Callback list. will be call during training 
+    callbacks_list = [checkpointer]
+    if ropl:
+        callbacks_list.append(reduce_lr)
+
+    hist = None
+    hist_file = "./_cnnet/history.p"
+    _saved = False
+    aborted_by_user = False
+
+    try:
+        hist = model.fit_generator(train_generator,\
+                    steps_per_epoch = hyper_params.steps_per_epoch,\
+                    validation_data = validation_generator,\
+                    validation_steps = hyper_params.nb_val_samples,\
+                    verbose=1,\
+                    epochs=hyper_params.epochs,\
+                    callbacks=callbacks_list, workers=5)
+        
+    except KeyboardInterrupt:
+        print("Training aborted by user")    
+        model.save('./_cnnet/model.h5')
+        print("Model saved.")
+        _saved = True
+        aborted_by_user = True
+        
+    if not aborted_by_user:
+        ## Save history to file
+        out_tr_file = open(hist_file, 'wb')
+        hist_container = {'acc': hist.history['acc'], 'val_acc': hist.history['val_acc'],\
+                      'loss': hist.history['loss'], 'val_loss': hist.history['val_loss']}
+        pickle.dump(hist_container, out_tr_file)
+        out_tr_file.close()
+    
+    if not _saved:
+        print("Training history saved.")
+        model.save('./_cnnet/model.h5')
+        print("Model saved.")
+
+
+if __name__ == '__main__':
+    hyper_params = namedtuple('HyperParameters', 'epochs batch learning_rate\
+                             nb_val_samples optimizer samples_per_epoch in_shape')
+
+    ## load data database from exported files 
+    samples = balance_data(["./exported/export_2017_07_23_12_23_46.csv",
+                            "./exported/export_2017_07_23_12_25_33.csv",
+                            "./exported/export_2017_07_23_12_25_43.csv"], False, reduce_factor=0.00)
+
+    # shuffle the dataframe
+    samples = shuffle(samples)
+
+    # split for training (90%) and test (10%)
+    train_samples, validation_samples = train_test_split(samples, test_size=0.1)
+
+    # declare generators
+    hyper_params.batch = 256
+    tr_generator = generator(train_samples, batch_size=hyper_params.batch)
+    train_generator = tr_generator
+    
+    val_generator = generator(validation_samples, batch_size=hyper_params.batch)
+    validation_generator = val_generator
+    
+    # set hyperparams
+    hyper_params.epochs = 10
+    hyper_params.steps_per_epoch = len(train_samples)
+    hyper_params.nb_val_samples = len(validation_samples)    
+    hyper_params.learning_rate = 1e-4
+
+    # Set image input size
+    hyper_params.in_shape = (16, 64, 3)
+    if is_gray_mode():
+        hyper_params.in_shape = (16, 64, 1)
+
+    # Call training routine!
+    train(hyper_params, ropl=False)
+
+
